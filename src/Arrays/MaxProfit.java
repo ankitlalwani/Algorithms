59c3738b079143c9ca0e23c433d463711ed95c3c
Ankit Lalwani <36794216+ankitlalwani@users.noreply.github.com>
2020-11-03 13:54:18 -0500

    LeetCode Solution
    P122

--- src/Arrays/MaxProfit.java
+++ src/Arrays/MaxProfit.java
@@ -1,5 +1,9 @@
 package Arrays;
-
+/*
+P122 - Say you have an array prices for which the ith element is the price of a given stock on day i.
+Design an algorithm to find the maximum profit. You may complete as many transactions as you
+like (i.e., buy one and sell one share of the stock multiple times).
+ */
 public class MaxProfit {
 
     public int maxProfit(int[] prices) {
